379887cc54e77f2ab534ae78a719ad26ffa42daf
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-08-21 20:28:33 -0400

    Add standard for lint checking

--- statsd_logger.js
+++ statsd_logger.js
@@ -1,25 +1,25 @@
-var colors = require('colors'),
-  dgram = require('dgram'),
-  server = dgram.createSocket('udp4');
+require('colors')
+var dgram = require('dgram')
+var server = dgram.createSocket('udp4')
 
-server.on('error', function(err) {
-  console.log('Server error:\n'.red + err.stack.red);
-  server.close();
-});
+server.on('error', function (err) {
+  console.log('Server error:\n'.red + err.stack.red)
+  server.close()
+})
 
-server.on('listening', function() {
-  var address = server.address();
-  console.log('Server listening on ' + address.address + ':' + address.port);
-});
+server.on('listening', function () {
+  var address = server.address()
+  console.log('Server listening on ' + address.address + ':' + address.port)
+})
 
-server.on('message', function(msg, rinfo) {
-  var msg_parts = msg.toString().split('\n');
-  for (var i = 0; i < msg_parts.length; i++) {
-    var parts = msg_parts[i].split(':');
+server.on('message', function (msg, rinfo) {
+  var msgParts = msg.toString().split('\n')
+  for (var i = 0; i < msgParts.length; i++) {
+    var parts = msgParts[i].split(':')
     // only split once...
-    parts = [parts.shift(), parts.join(":")];
-    console.log('StatsD Metric: ' + parts[0].blue + ' ' + parts[1].green);
+    parts = [parts.shift(), parts.join(':')]
+    console.log('StatsD Metric: ' + parts[0].blue + ' ' + parts[1].green)
   }
-});
+})
 
-server.bind(process.env.PORT || 8125);
+server.bind(process.env.PORT || 8125)
